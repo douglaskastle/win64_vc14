3a9891d8063dde78d567c525a21bbe54041f3aa2
LazyDodo <LazyDodo@954f8c5b-7b00-dc11-b283-0030488c597c>
2018-08-28 00:14:24 +0000

    windows: boost 1.68.0

--- trunk/lib/win64_vc14/boost/include/boost/geometry/algorithms/detail/relate/result.hpp
+++ trunk/lib/win64_vc14/boost/include/boost/geometry/algorithms/detail/relate/result.hpp
@@ -1,9 +1,10 @@
 // Boost.Geometry (aka GGL, Generic Geometry Library)
 
 // Copyright (c) 2007-2015 Barend Gehrels, Amsterdam, the Netherlands.
+// Copyright (c) 2017 Adam Wulkiewicz, Lodz, Poland.
 
-// This file was modified by Oracle on 2013, 2014, 2015.
-// Modifications copyright (c) 2013-2015 Oracle and/or its affiliates.
+// This file was modified by Oracle on 2013-2016.
+// Modifications copyright (c) 2013-2016 Oracle and/or its affiliates.
 
 // Contributed and/or modified by Adam Wulkiewicz, on behalf of Oracle
 
@@ -15,6 +16,7 @@
 #define BOOST_GEOMETRY_ALGORITHMS_DETAIL_RELATE_RESULT_HPP
 
 #include <cstddef>
+#include <cstring>
 
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/at.hpp>
@@ -24,6 +26,7 @@
 #include <boost/mpl/is_sequence.hpp>
 #include <boost/mpl/next.hpp>
 #include <boost/static_assert.hpp>
+#include <boost/throw_exception.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/type_traits/integral_constant.hpp>
 
@@ -131,9 +134,6 @@ public:
     static const bool interrupt = false;
 
     matrix_handler()
-    {}
-
-    matrix_handler(Matrix const&)
     {}
 
     result_type const& result() const
@@ -269,7 +269,7 @@ private:
                          || ( c >= '0' && c <= '9' );
         if ( !is_valid )
         {
-            throw geometry::invalid_input_exception();
+            BOOST_THROW_EXCEPTION(geometry::invalid_input_exception());
         }
     }
 
@@ -603,10 +603,6 @@ public:
     typedef bool result_type;
 
     bool interrupt;
-
-    inline mask_handler()
-        : interrupt(false)
-    {}
 
     inline explicit mask_handler(Mask const& m)
         : interrupt(false)
